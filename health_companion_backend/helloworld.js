441e515abda7b46dc5111f6c9181b6f1e81439fa
Ssureshbabu48 <Ssureshbabu48@gmail.com>
2019-05-08 16:56:48 -0400

    Removed hello world

--- health_companion_backend/helloworld.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const showme = express();
-
-showme.get('/', (req, res) => {
-	res.send("Hellooooo World!");
-});
-
-showme.listen(3000, () => console.log('Server active on port '+ port + '.'));
